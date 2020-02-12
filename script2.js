493ea26a6826a384dbbfdeebf805e62a81c0568c
Ikraam <ikraamghoor@gmail.com>
2020-02-12 15:43:08 +0200

    fixed bugs and cleaned debugging code

--- script2.js
+++ script2.js
@@ -42,7 +42,7 @@ numberButtons.forEach((button) => {
 	button.addEventListener('click', () => {
 		try {
 			// eslint-disable-next-line no-empty
-			if (displayValue.includes('.') && button.textContent === '.') {
+			if (screen.textContent.includes('.') && button.textContent === '.') {
 			} else displayValue += `${button.textContent}`;
 			replaceScreen(displayValue);
 			equalsFlag = 0;
@@ -62,41 +62,31 @@ clear.addEventListener('click', () => {
 
 const solve = document.querySelector('#buttonEquals');
 solve.addEventListener('click', () => {
-	// console.log(displayValue);
+	// eslint-disable-next-line no-empty
 	if (equalsFlag === 1) {
-		displayValue = '';
-		replaceScreen(displayValue);
 	} else {
 		equalsFlag = 1;
 		// eslint-disable-next-line no-useless-escape
 		const solveArray = displayValue.split(/([^\.0-9]=?)/);
-		// console.log('TCL: solveArray', solveArray);
 		for (let i = 0; i < solveArray.length; i += 1) {
-			// if (Number.isNaN(Number(solveArray[i]))) {
 			if (solveArray[i] === '*' || solveArray[i] === '/') {
-				// console.log('TCL: currentNo', solveArray[i]);
 				solveArray[i - 1] = operate(
 					parseFloat(solveArray[i - 1]),
 					parseFloat(solveArray[i + 1]),
 					solveArray[i]
-				); // operating on previous and next value
+				);
 				solveArray.splice(i, 2);
-				// console.log('TCL: solveArray', solveArray);
 				i -= 2;
 			}
 		}
-
 		for (let i = 0; i < solveArray.length; i += 1) {
-			// if (Number.isNaN(Number(solveArray[i]))) {
 			if (solveArray[i] === '+' || solveArray[i] === '-') {
-				// console.log('TCL: currentNo', solveArray[i]);
 				solveArray[i - 1] = operate(
 					parseFloat(solveArray[i - 1]),
 					parseFloat(solveArray[i + 1]),
 					solveArray[i]
-				); // operating on previous and next value
+				);
 				solveArray.splice(i, 2);
-				// console.log('TCL: solveArray', solveArray);
 				i -= 2;
 			}
 		}
